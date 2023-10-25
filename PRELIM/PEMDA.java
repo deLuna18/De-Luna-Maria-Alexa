f83ad6ea9e9aae38bfe90778c17ca04c34fade36
Daramdami <113959920+deLuna18@users.noreply.github.com>
2023-10-25 06:02:08 +0100

    Add files via upload

---
+++ PRELIM/PEMDA.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;                 
+public class PEMDA
+{
+    public static void main(String args[])
+  {
+    int first, second, add, multiply;
+    float divide;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter Two Numbers : ");
+    first = scanner.nextInt();
+    second = scanner.nextInt();
+
+    add = first + second;
+    multiply = first * second;
+    divide = (float) first / second;
+
+    System.out.println("Sum = " + add);
+    System.out.println("Multiplication = " + multiply);
+    System.out.println("Division = " + divide);
+  }
+}
